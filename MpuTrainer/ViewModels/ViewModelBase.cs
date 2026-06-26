ee83300f9f71527c1389f8c36c2cc417d491d94d
EjupXhemajli <ejup.xhemajli@mpu-bfk.de>
2026-06-27 00:04:18 +0200

    version19

--- MpuTrainer/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace MpuTrainer.ViewModels;
-
-/// <summary>Basisklasse aller ViewModels (Property-Change-Benachrichtigung).</summary>
-public abstract partial class ViewModelBase : ObservableObject
-{
-}
